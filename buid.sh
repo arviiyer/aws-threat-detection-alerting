c6fa21af0c8712fa0d610204d466fd14a4983808
Arvind Iyer <rbarvind04@gmail.com>
2025-05-21 23:18:58 -0400

    Updated build script to support isolation lambda function

--- buid.sh
+++ buid.sh
@@ -3,18 +3,33 @@
 set -e # Stop on error
 
 # Clean previous builds
-rm -rf build lambda_function.zip
+rm -rf detection guardduty_formatter.zip
+rm -rf respone ec2_isolation_handler.zip
 
-# Create build directory
-mkdir -p build
+# Create detection directory
+mkdir -p detection
 
-# Install dependencies to build/
-pip install -r requirements.txt -t build/
+# Install dependencies to detection/
+pip install -r requirements.txt -t detection/
 
-# Copy your Lambda function code
-cp lambda_function.py build/
+# Copy Lambda function code
+cp guardduty_formatter.py detection/
 
-# Package everything from inside build/
-cd build
-zip -r ../lambda_function.zip .
+# Package everything from inside detection/
+cd detection
+zip -r ../guardduty_formatter.zip .
+cd ..
+
+# Create respone directory
+mkdir -p response
+
+# Install dependencies to respone/
+# No dependencies so far for response. Add here if it changes
+
+# Copy lambda function code
+cp ec2_isolation_handler.py response
+
+# Package everything from inside response/
+cd response
+zip -r ../ec2_isolation_handler.zip .
 cd ..
